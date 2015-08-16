9af213846c6796ead907fc5ce7be358f162845db
etheleon <picy2k@gmail.com>
2015-08-16 17:44:47 +0800

    w STOP CODON, and full CODON table

--- golang/chap1/exercise1-1.go
+++ golang/chap1/exercise1-1.go
@@ -2,52 +2,50 @@
 Task1: Write a program that lists all the DNA sequences that encode a given protein sequence
 */
 
-/*
-codon table
-    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
-    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
-    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
-
-
-    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-    "CAU":"H", "CAC":"H",
-    "AUU":"I", "AUC":"I", "AUA":"I",
-    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
-    "AGU":"S", "AGC":"S",
-    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
-    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",
-
-*/
-
-/*
-eg. protein sequence MRQFL
-*/
-
 package main
 
 import "fmt"
 import "strings"
 
 var codonTable = map[string][]string{
-	"m": []string{"AUG"},
-	"r": []string{"CGG", "CGA", "CGC", "CGU", "AGG", "AGA"},
-	"q": []string{"CAG", "CAA"},
-	"f": []string{"UUU", "UUC"},
-	"l": []string{"UUA", "UUG", "CUU", "CUC", "CUA", "CUG"},
+	"stop": []string{"UAA", "UAG", "UGA"},
+	"e":    []string{"GAA", "GAG"},
+	"d":    []string{"GAU", "GAC"},
+	"g":    []string{"GGU", "GGC", "GGA", "GGG"},
+	"v":    []string{"GUU", "GUC", "GUA", "GUG"},
+	"t":    []string{"ACU", "ACC", "ACA", "ACG"},
+	"i":    []string{"AUU", "AUC", "AUA"},
+	"h":    []string{"CAU", "CAC"},
+	"a":    []string{"GCU", "GCC", "GCA", "GCG"},
+	"w":    []string{"UGG"},
+	"c":    []string{"UGU", "UGC"},
+	"y":    []string{"UAU", "UAC"},
+	"s":    []string{"UCU", "UCC", "UCA", "UCG", "AGU", "AGC"},
+	"m":    []string{"AUG"},
+	"n":    []string{"AAU", "AAC"},
+	"r":    []string{"CGG", "CGA", "CGC", "CGU", "AGG", "AGA"},
+	"p":    []string{"CCU", "CCC", "CCA", "CCG"},
+	"q":    []string{"CAG", "CAA"},
+	"f":    []string{"UUU", "UUC"},
+	"l":    []string{"UUA", "UUG", "CUU", "CUC", "CUA", "CUG"},
+	"k":    []string{"AAA", "AAG"},
 }
 
 var peptide = []string{"M", "R", "F", "L"}
 
+var grandList = []string{}
+
 func recurse(prevSeq string, aa []string) int {
 	if len(aa) == 0 {
-		fmt.Println("Seq: ", prevSeq)
+		for _, stopCodon := range codonTable["stop"] {
+			finalSeq := strings.Join(append([]string{prevSeq}, stopCodon), " ")
+			grandList = append(grandList, finalSeq)
+		}
+
 		return 1
 	} else {
 		for _, codon := range codonTable[strings.ToLower(aa[0])] {
-			newSeq := strings.Join(append([]string{prevSeq}, codon), "")
+			newSeq := strings.Join(append([]string{prevSeq}, codon), " ")
 			recurse(newSeq, aa[1:])
 		}
 	}
@@ -56,10 +54,10 @@ func recurse(prevSeq string, aa []string) int {
 
 func main() {
 	recurse("", peptide)
+	for i, seq := range grandList {
+		i = i + 1
+		fmt.Print("Seq ")
+		fmt.Printf("%0.2d", i)
+		fmt.Println(":", seq)
+	}
 }
-
-//for key, value := range codonTable {
-//for _, element := range value {
-//fmt.Println(key, element)
-//}
-//}
